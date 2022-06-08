c6764d3781c7fd2bdb56afd1e26dc55fbc380930
bogyeong <boggyhint@gmail.com>
2022-06-08 11:03:06 +0900

    edit test

---
+++ src/test/java/tk/meokeobolre/meokeobolre/MeokeobolReApplicationTest.java
@@ -0,0 +1,12 @@
+package tk.meokeobolre.meokeobolre;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MeokeobolReApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
